3ec31be7208548f0032bc7e8501d2cde1b2aec2a
KacperWTrzecki <86572468+KacperWTrzecki@users.noreply.github.com>
2021-06-29 22:51:13 +0100

    Add files via upload

--- Content/js/script.js
+++ Content/js/script.js
@@ -3,3 +3,21 @@
     var n = d.getFullYear() + "  ";
     document.getElementById("date").innerHTML = n;
 });
+
+$(".project-panel").each(function (index) {
+    $(this).on("click", function () {
+        var modal = document.getElementById("imgModal");
+        var folder = modal.getAttribute("folder");
+        var modalImg = document.getElementById("img");
+        var img = this.getAttribute("imgId");
+
+        modal.style.display = "block";
+        modalImg.setAttribute('src', 'Content/images/' + folder + '/' + img + '.jpg');
+
+        var span = document.getElementsByClassName("close")[0];
+
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+    });
+});
